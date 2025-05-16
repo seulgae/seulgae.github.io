36591003ccbb1fb77a9fa3d7927ac4a7ed496329
seulgae <r1605866@naver.com>
2025-05-16 13:49:39 +0900

    사이드 프로젝트 임시 활성화

--- src/pages/SideProjects.jsx
+++ src/pages/SideProjects.jsx
@@ -20,20 +20,20 @@ const sideProjects = [
   // },
   {
     title: "FM Manager",
-    date: "2022.11, 리뉴얼 2025.04.18 ~ ",
+    date: "2022.11, 리뉴얼 2025.06(예정) ",
     description: "구장 예약부터 동호회 소통까지, 풋살을 즐기는 사람들을 위한 올인원 플랫폼 사이트.",
     image: "/FMLogo.png",
     link: "https://github.com/seulgae/fmBootReact",
     oldLink: "https://github.com/seulgae/fmBoot.git"
   },
-  // {
-  //   title: "OneDayClass",
-  //   date: "2021.10, 리뉴얼 2025.06 ~ ",
-  //   description: "수강생을 연결해주는 일일 체험 학습 플랫폼 사이트, 누구나 쉽게 클래스를 등록하고 리뷰로 소통할 수 있는 공간.",
-  //   image: "/OneDayClass.png",
-  //   link: "https://github.com/seulgae/OneDayClass.git",
-  //   oldLink: "https://github.com/seulgae/Proj_OnedayClass"
-  // },
+  {
+    title: "OneDayClass",
+    date: "2021.10, 리뉴얼 2025.07(예정) ",
+    description: "수강생을 연결해주는 일일 체험 학습 플랫폼 사이트, 누구나 쉽게 클래스를 등록하고 리뷰로 소통할 수 있는 공간.",
+    image: "/OneDayClass.png",
+    link: "https://github.com/seulgae/OneDayClass.git",
+    oldLink: "https://github.com/seulgae/Proj_OnedayClass"
+  },
 ];
 
 const SideProjects = () => {
